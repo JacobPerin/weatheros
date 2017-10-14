64410c921ddae4a6468ad6c1ba1ea85741560de1
perin <perin@iastate.edu>
2017-10-14 16:02:13 -0500

    Broke code up w/ ES6 Arrow functions :: Easier reading

--- weatheros/src/views/WeatherList.js
+++ weatheros/src/views/WeatherList.js
@@ -7,37 +7,54 @@ function WeatherList(props) {
 		props.onRemove(e.target.id)
 	}
 
+	const displayA = (city, dayTemp) => ((
+		<span className = "sharedSpan">
+			<div className = "shareA city">
+				<span className = "t"></span>
+				<div className = "shareB">{city}</div>
+			</div>
+			<div className = "shareA">
+				<span className = "t"></span>								
+				<div className = "shareB">{dayTemp}</div>
+			</div>
+		</span>	
+	));
+
+	const displayB = (values) => ((
+		<span className = "hourlyList">
+			<nav>
+				<ul>
+					{values.map(function(listValue, index){
+						return <li key = {index}>{listValue}</li>
+					})}
+				</ul>
+			</nav>
+		</span>
+	));
+
+	/* Display C --> Bar graph :: to large to place here must be in own file */
+
+
+	const displayD = (id) => ((
+		<span
+				className = "exit"
+				id={id}
+	    		onClick={onClick}>
+	    </span>
+	));
+
 	const WeatherItems = [...props.weatherItems.values()].reverse().map(WeatherItem =>
 			(
 				<li className = "a" key={WeatherItem.id} >
 					<div className = "fill">
-						<span className = "sharedSpan">
-							<div className = "shareA city">
-								<span className = "t"></span>
-								<div className = "shareB">{WeatherItem.temp}</div>
-							</div>
-							<div className = "shareA">
-								<span className = "t"></span>								
-								<div className = "shareB">80</div>
-							</div>
-						</span>
-						<span className = "hourlyList">
-							<nav>
-								<ul>
-									{WeatherItem.tempHourly.map(function(listValue, index){
-										return <li key = {index}>{listValue}</li>
-									})}
-								</ul>
-							</nav>
-						</span>
+						{displayA(WeatherItem.temp, '000')}
+						{displayB(WeatherItem.tempHourly)}
 						<span className = "barGraph"></span>
-						<span
-								className = "exit"
-								id={WeatherItem.id}
-			            		onClick={onClick}></span>
+						{displayD(WeatherItem.id)}
 					</div>
 			    </li>
 	));
+	
 	return (
 		<section id="list">
     	<ul id="weatherList">
